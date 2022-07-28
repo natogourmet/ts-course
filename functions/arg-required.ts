bcde418478f95ca5fd03e2609db92ca575207aae
natogourmet <natogourmet@hotmail.com>
2022-07-28 10:36:48 -0500

    28-required-args

---
+++ functions/arg-required.ts
@@ -0,0 +1,10 @@
+(()=> {
+
+  const fullName = ( firstName: string, lastName: string ): string => {
+    return `${ firstName } ${ lastName }`;
+  }
+
+  console.log( fullName( 'Tony', 'Stark' ) );
+  
+
+})();
