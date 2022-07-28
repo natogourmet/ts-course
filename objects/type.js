43f30cc476a64477a6c55d051e9c13541e310804
natogourmet <natogourmet@hotmail.com>
2022-07-28 12:58:33 -0500

    39-types

---
+++ objects/type.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    let jojo = {
+        name: 'Joseph',
+        age: 21,
+        powers: ['Hamon', 'Smart', 'Strong', 'Funny']
+    };
+    let jojo2 = {
+        name: 'Jotaro',
+        age: 17,
+        powers: ['Stand', 'Smart', 'Strong']
+    };
+})();
