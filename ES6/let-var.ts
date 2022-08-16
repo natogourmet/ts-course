74acdbc0315934415c5719e665a0a54f98805aa9
natogourmet <natogourmet@hotmail.com>
2022-08-16 15:17:55 -0500

    51-let-var

---
+++ ES6/let-var.ts
@@ -0,0 +1,18 @@
+(() => {
+
+  // console.log(a);  
+  var a = 10;
+
+  let b = 20;
+  console.log(20);
+
+  const getName = () => {
+    console.log('My Name');
+  }
+
+
+  // Error
+  // getName = () => {console.log('My Name 2')};
+
+  
+})();
