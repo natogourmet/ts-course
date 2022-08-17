e9d5ab84bd1aace50b8821831e71b702ddcbb0a8
natogourmet <natogourmet@hotmail.com>
2022-08-17 11:21:00 -0500

    58-ts-classes

--- main.js
+++ main.js
@@ -1,56 +1,13 @@
 "use strict";
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
+(() => {
+    class Avenger {
+        constructor(name, team, realName) {
+            this.name = name;
+            this.team = team;
+            this.realName = realName;
         }
-    return t;
-};
-(() => {
-    const avengers = {
-        nick: "Samuel Jackson",
-        ironman: "Robert Downey Jr",
-        vision: "Paul Bettany",
-        power: 1240.50,
-        active: true,
-    };
-    const printAvengers = (_a) => {
-        var { nick, ironman } = _a, rest = __rest(_a, ["nick", "ironman"]);
-        console.log(nick, ironman, rest);
-    };
-    printAvengers(avengers);
-    const avengersArr = ['Cap', 'Thor', 'Hulk', 'Ironman'];
-    const [cap, thor, hulk, ironman] = avengersArr;
-})();
-(() => {
-    const ironman = {
-        name: "Tony Stark",
-        weapon: "Mark 50"
-    };
-    const thor = {
-        name: "Thor",
-        weapon: "Mjolnir"
-    };
-    const captainAmerica = {
-        name: "Steve Rogers",
-        weapon: "Shield"
-    };
-    const avengers = [ironman, thor, captainAmerica];
-    for (const avenger of avengers) {
-        console.log(avenger);
     }
-});
-();
-(() => {
-    var a = 10;
-    let b = 20;
-    console.log(20);
-    const getName = () => {
-        console.log('My Name');
-    };
+    Avenger.avgAge = 35;
+    const antman = new Avenger('Antman', 'Capi', 'Scott Lang');
 })();
 //# sourceMappingURL=main.js.map
